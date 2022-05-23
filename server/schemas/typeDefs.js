de4f105061d10e79c825864766cfc4878d96ecb7
dieres <dieres2010@gmail.com>
2022-05-23 19:18:25 -0400

    GraphQL queries and navigation for other pages enhancement Implemented

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,14 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -19,14 +25,12 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+
+  type Auth {
+    token: ID!
+    user: User
   }
+
   type Query {
     me: User
     users: [User]
@@ -34,6 +38,7 @@ const typeDefs = gql`
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
@@ -41,11 +46,6 @@ const typeDefs = gql`
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
   }
-  type Auth {
-    token: ID!
-    user: User
-  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
